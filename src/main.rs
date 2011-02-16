ffea033c4bf4f1461739b00a2f306bbc6426fefb
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2011-02-16 00:00:00 +0000

    Speed improvements + 1.21.20.22 beta support

--- src/main.rs
+++ src/main.rs
@@ -16,18 +16,24 @@ use console::style;
 use materialbin::{CompiledMaterialDefinition, MinecraftVersion};
 use scroll::Pread;
 use zip::{
+    read::ZipFile,
     write::{ExtendedFileOptions, FileOptions},
     ZipArchive, ZipWriter,
 };
+use zune_inflate::DeflateOptions;
 #[derive(Parser)]
 #[clap(name = "Material Updater", version = "0.1.1")]
 #[command(version, about, long_about = None, styles = get_style())]
 struct Options {
-    /// Shader pack to update
+    /// Shader pack fild to update
     #[clap(required = true)]
     file: PathBuf,
 
-    /// Version of output
+    /// Output zip compression level
+    #[clap(short, long)]
+    zip_compression: Option<u32>,
+
+    /// Output version
     #[clap(short, long, required = true)]
     target_verion: MVersion,
 
@@ -38,6 +44,7 @@ struct Options {
 // Hack for clap support
 #[derive(ValueEnum, Clone)]
 enum MVersion {
+    V1_21_20,
     V1_20_80,
     V1_19_60,
     V1_18_30,
@@ -48,6 +55,7 @@ impl MVersion {
             Self::V1_20_80 => MinecraftVersion::V1_20_80,
             Self::V1_19_60 => MinecraftVersion::V1_19_60,
             Self::V1_18_30 => MinecraftVersion::V1_18_30,
+            Self::V1_21_20 => MinecraftVersion::V1_21_20,
         }
     }
 }
@@ -69,21 +77,22 @@ fn main() -> anyhow::Result<()> {
     let mut outzip = ZipWriter::new(output);
     let mut translated_shaders = 0;
     for index in 0..zip.len() {
-        let mut file = zip.by_index(index)?;
+        let mut file = zip.by_index_raw(index)?;
         if !file.name().ends_with(".material.bin") {
             outzip.raw_copy_file(file)?;
             continue;
         }
         print!("Processing file {}", style(file.name()).cyan());
-        let mut data = Vec::with_capacity(file.size().try_into()?);
-        file.read_to_end(&mut data)?;
+        let mut data = fast_decompress(&mut file)?;
         let material = match read_material(&data) {
             Ok(material) => material,
             Err(_) => {
                 anyhow::bail!("Material file {} is invalid for all versions", file.name());
             }
         };
-        outzip.start_file(file.name(), FileOptions::<ExtendedFileOptions>::default())?;
+        let file_options = FileOptions::<ExtendedFileOptions>::default()
+            .compression_level(opts.zip_compression.and_then(|v| Some(v.into())));
+        outzip.start_file(file.name(), file_options)?;
         material.write(&mut outzip, mcver)?;
         translated_shaders += 1;
     }
@@ -95,16 +104,26 @@ fn main() -> anyhow::Result<()> {
     );
     Ok(())
 }
+fn fast_decompress(zipfile: &mut ZipFile) -> anyhow::Result<Vec<u8>> {
+    let mut output = Vec::with_capacity(zipfile.size().try_into()?);
+    let _data = zipfile.read_to_end(&mut output)?;
+    let options = DeflateOptions::default().set_size_hint(zipfile.size().try_into()?);
+    let mut decoder = zune_inflate::DeflateDecoder::new_with_options(&output, options);
+    let decompressed = decoder.decode_deflate()?;
+    Ok(decompressed)
+}
 fn read_material(data: &[u8]) -> anyhow::Result<CompiledMaterialDefinition> {
-    for version in [
-        MinecraftVersion::V1_20_80,
-        MinecraftVersion::V1_19_60,
-        MinecraftVersion::V1_18_30,
-    ] {
+    for version in materialbin::ALL_VERSIONS {
+        println!("Trying {version}");
         if let Ok(material) = data.pread_with(0, version) {
             println!(" [{version}]");
             return Ok(material);
         }
+        println!(
+            "error: {}",
+            data.pread_with::<CompiledMaterialDefinition>(0, version)
+                .unwrap_err()
+        );
     }
 
     anyhow::bail!("Material file is invalid");
